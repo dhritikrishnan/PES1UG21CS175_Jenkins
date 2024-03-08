428a9953da4b5b1f67aff33c2540963a0dfe27b9
Dhriti Rajesh Krishnan <92797979+dhritikrishnan@users.noreply.github.com>
2024-03-08 13:23:10 +0530

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello,Main\n";
   cout << "Hello, Jenkins\n";
   cout << "Pipeline Built\n";
-  cout << "My srn is PES1UG21CS175\n" ;
+  cout << "My srn is PES1UG21CS175\n" 
   return 0;
 }
